14d16551f9c071792389b6e1aa06c91ddea542af
slpark8888 <slpark8888@gmail.com>
2022-02-16 12:38:35 +0900

    day31

--- day31/website/app.py
+++ day31/website/app.py
@@ -8,13 +8,13 @@ def index():
 @app.route('/season/')
 def season():
     season = "겨울"
-    seasons = ["봄","여름","가을","겨울"]
+    seasons = ["봄", "여름", "가을", "겨울"]
     return render_template('season.html',
                            season=season, seasons=seasons) #키의 값
 
 @app.route('/loop_index/', methods = ['GET'])
 def loop_index():
-    items = ['a','b','c','d']
+    items = ['a', 'b', 'c', 'd']
     return render_template('loop_index.html', items=items)
 
 app.run()
